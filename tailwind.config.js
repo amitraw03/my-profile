c8b01a83150a81c1ca72242300c258bd277d11fe
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-11-17 16:46:48 +0530

    React based portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all file types you use
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
